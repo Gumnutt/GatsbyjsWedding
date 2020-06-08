e67c3ae3c6bc343df347688d45d987e72be38080
gumnutt <brynn@annex.com.au>
2020-06-08 12:07:45 +1000

    home page edits

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../assets/sass/main.scss';
+import { Link } from 'gatsby';
 const Home = () => (
   <section id="intro" className="wrapper style1 fullscreen fade-up">
     <div className="inner inner-no-margin">
-      <h1>We're getting hitched baby</h1>
+      <h1>We got hitched baby</h1>
       <p>8th March 2020</p>
+      <Link className="button primary gallery" to="/gallery/" style={{background:'white', color:'#333'}}>Click here to see photos from the day</Link>
     </div>
   </section>
 );
